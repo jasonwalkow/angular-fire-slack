430a145aa55ed19a60f001d6adceb6615d48615f
Jason Walkow <jason.walkow1@gmail.com>
2015-10-20 18:45:14 -0700

    initial commit

---
+++ app/channels/channels.controller.js
@@ -0,0 +1,25 @@
+angular.module('angularfireSlackApp')
+	.controller('ChannelsCtrl', function($state, Auth, Users, profile, channels) {
+		var channelsCtrl = this;
+		channelsCtrl.profile = profile;
+		channelsCtrl.channels = channels;
+		channelsCtrl.getDisplayName = Users.getDisplayName;
+		channelsCtrl.getGravatar = Users.getGravatar;
+		channelsCtrl.users = Users.all;
+		Users.setOnline(profile.$id);
+		channelsCtrl.logout = function(){
+		  channelsCtrl.profile.online = null;
+		  channelsCtrl.profile.$save().then(function(){
+		    Auth.$unauth();
+		    $state.go('home');
+		  });
+		};
+		channelsCtrl.newChannel = {
+			name: ''
+		};
+		channelsCtrl.createChannel = function(){
+		  channelsCtrl.channels.$add(channelsCtrl.newChannel).then(function(ref){
+		    $state.go('channels.messages', {channelId: ref.key()});
+		  });
+		};
+	});
